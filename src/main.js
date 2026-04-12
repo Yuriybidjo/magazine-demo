0fe8013c9fc5f82a4975a9649f5f0b0bc5a7d945
Yuriybidjo <yuriybidjo@gmail.com>
2026-04-12 22:22:28 +0300

    changed html js and more..

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
 import "./style.css";
+
+const openBtn = document.getElementById('open-menu');
+const mobileNav = document.getElementById('mobile-nav');
+const hamburger = document.getElementById('hamburger')
+
+// openBtn.addEventListener('click', () => {
+//   mobileNav.classList.toggle('translate-y-full');
+//   hamburger.classList.toggle('ri-close-large-line')
+// });
+
+openBtn.addEventListener('click', () => {
+  // Toggle the X-axis translation
+  mobileNav.classList.toggle('translate-x-full');
+
+  // Toggle the icon from menu to close
+  hamburger.classList.toggle('ri-close-large-line');
+  hamburger.classList.toggle('ri-menu-line'); // Ensure the original icon is swapped
+});
